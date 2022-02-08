c69fadc75dddc6a05b536f37aa7af160cedcb941
Taska <77rex7@gmail.com>
2022-02-08 05:16:54 +0300

    init

---
+++ app/src/main/java/com/example/myapplicationjava/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplicationjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.myapplicationjava.adapter.CategoryAdapter;
+import com.example.myapplicationjava.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1,"title1"));
+        categoryList.add(new Category(2,"title2"));
+        categoryList.add(new Category(3,"title3"));
+        categoryList.add(new Category(4,"title4"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
+
+    }
+}
